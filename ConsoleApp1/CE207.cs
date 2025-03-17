c5c03a57eda2a5ca300eff7a3db5036f08d9a97d
Phelanhik <phelanhik@ya.ru>
2025-03-17 21:14:00 +0300

    Разделил классы по разным файлам, поменял чуть синтаксис

---
+++ ConsoleApp1/CE207.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class CE207 : Counter
+    {
+
+        public CE207(int serialnumber, int adress, string password) : base(serialnumber, adress, password, 4) { }
+
+    }
+    
+}
---
+++ ConsoleApp1/CE208.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class CE208 : Counter
+    {
+        public CE208(int serialnumber, int adress, string password) : base(serialnumber, adress, password, 8) { }
+
+    }
+}
